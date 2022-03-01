55cb773cd9731775ae4fd0ea895e9e4e90c827c0
bkielczyk <bkielczyk255@technikumkreatywne.pl>
2022-03-01 11:07:02 +0100

    Liczby pierwsze

---
+++ liczby_pierwsze/liczby_pierwsze.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int ile;
+
+    cout << "Ile liczb: ";
+    cin >> ile;
+
+    int liczba = 2;
+    int pierwsze = 0;
+
+    while (pierwsze < ile) {
+        bool pierwsza = 1;
+        for (int j = 2; j < liczba; j++) {
+            if (liczba % j == 0) {
+                pierwsza = 0;
+            }
+        }
+        if (pierwsza == 1) {
+            cout << liczba << ", ";
+            pierwsze++;
+        }
+        liczba++;
+    }
+}
