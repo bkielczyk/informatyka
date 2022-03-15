325f2b7b30a5b1ff6f9788755ceecffe1fe0ef80
bkielczyk <bkielczyk255@technikumkreatywne.pl>
2022-03-15 10:53:49 +0100

    NWD i NWW

---
+++ nwd_i_nww/nwd_i_nww.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cout << "Podaj a: ";
+    cin >> a;
+    cout << "Podaj b: ";
+    cin >> b;
+    int iloczyn = a * b;
+    while (a != b) {
+        if (a < b) {
+            b = b - a;
+        }
+        else {
+            a = a - b;
+        }
+    }
+    cout << "NWD(a,b): " << a << endl;
+    int nww = iloczyn / a;
+    cout << "NWW(a,b): " << nww;
+}
